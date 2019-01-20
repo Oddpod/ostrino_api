f1d0d9ac67cf94ba7b35735d9c9e5baf4e4e3be1
Odd Harald Sjursen Sande <oddh123@gmail.com>
2019-01-20 18:38:41 +0100

    ...

--- OSTrino_API/wsgi.py
+++ OSTrino_API/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+import os
+
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OSTrino_API.settings.production")
 
 application = get_wsgi_application()
