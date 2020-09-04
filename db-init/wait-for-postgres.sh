328360464ee8f8681b414f84504a29caaa080a73
Oddpod <oddh123@gmail.com>
2020-09-04 20:51:13 +0200

    asdasdas

--- db-init/wait-for-postgres.sh
+++ db-init/wait-for-postgres.sh
@@ -6,7 +6,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U $DB_USER -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
