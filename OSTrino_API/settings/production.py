b38b2dd934750b29418ee6441828cdb56214e3be
Oddpod <oddh123@gmail.com>
2020-09-06 20:25:49 +0200

    cleanup production and fix origin

--- OSTrino_API/settings/production.py
+++ OSTrino_API/settings/production.py
@@ -1,25 +1,6 @@
 from os import environ
 from .base import *
 
-# Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
-from django.core.exceptions import ImproperlyConfigured
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': environ.get('DB_NAME'),
-        'USER': environ.get('DB_USER'),
-        'PASSWORD': environ.get('DB_PASSWORD'),
-        'HOST': environ.get('SQL_HOST'),
-        'PORT': environ.get('SQL_PORT'),
-    }
-}
-
-CORS_ALLOWED_ORIGINS = [
-    environ.get('FRONTEND_ORIGIN')
-]
-
 def get_env_setting(setting):
     """ Get the environment setting or return exception """
     try:
@@ -27,3 +8,22 @@ def get_env_setting(setting):
     except KeyError:
         error_msg = "Set the %s env variable" % setting
     raise ImproperlyConfigured(error_msg)
+
+# Normally you should not import ANYTHING from Django directly
+# into your settings, but ImproperlyConfigured is an exception.
+from django.core.exceptions import ImproperlyConfigured
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_setting('DB_NAME'),
+        'USER': get_env_setting('DB_USER'),
+        'PASSWORD': get_env_setting('DB_PASSWORD'),
+        'HOST': get_env_setting('SQL_HOST'),
+        'PORT': get_env_setting('SQL_PORT'),
+    }
+}
+
+CORS_ORIGIN_WHITELIST = (
+    get_env_setting('FRONTEND_ORIGIN'),
+)
