5422d5eab1b810bc603f2d48524c33bf8f4d7283
Oddpod <oddh123@gmail.com>
2020-09-12 12:37:15 +0200

    add correct host

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -7,7 +7,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
@@ -20,7 +20,6 @@ until postgres_ready; do
 done
 
 >&2 echo "Postgres is up - continuing..."
-exec $cmd
 
 python3 manage.py collectstatic --noinput
-exec "$@"
+exec $cmd
