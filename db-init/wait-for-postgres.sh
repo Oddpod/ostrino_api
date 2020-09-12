32101c82461bd8bbb3eb6152ef92042322c8c0b6
Oddpod <oddh123@gmail.com>
2020-09-12 19:41:15 +0200

    Change name db to postgres

--- db-init/wait-for-postgres.sh
+++ db-init/wait-for-postgres.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 set -e
-
-host="$1"
-shift
 cmd="$@"
-until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USER" -d $DB_NAME -c '\q'; do
+
+function postgres_ready(){
+python << END
+import sys
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
